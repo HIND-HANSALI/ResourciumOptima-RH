11eac2c2f0bd8f90432a7d899298f456044e4f26
HIND-HANSALI <hindhsl111@gmail.com>
2023-10-29 13:41:20 +0100

    update tests

--- src/test/java/com/resourciumoptimarh/services/EquipementServiceTest.java
+++ src/test/java/com/resourciumoptimarh/services/EquipementServiceTest.java
@@ -3,31 +3,36 @@ package com.resourciumoptimarh.services;
 import com.resourciumoptimarh.model.Equipement;
 import com.resourciumoptimarh.model.Etat;
 import com.resourciumoptimarh.repositories.EquipementRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
 class EquipementServiceTest {
 
-    EquipementService equipementService=new EquipementService();
+    EquipementService equipementService;
 
-    private EquipementRepository equipementRepository=new EquipementRepository();
+    EquipementRepository equipementRepository;
+
+    @BeforeEach
+    void setUp() {
+        equipementRepository = mock(EquipementRepository.class);
+        equipementService = new EquipementService();
+    }
 
     @Test
-    public void test_save_new_equipement() {
+    public void test_save_new_equipement_with_null_etat() {
 
         Equipement equipement = new Equipement("Test Equipement", "Test Type", new Date(), new Date());
 
 
-        equipementService.createEquipement(equipement);
-
-
-        Equipement savedEquipement = equipementRepository.getById(equipement.getId());
-
-        // Assert that the saved Equipement object is not null
-        assertNotNull(savedEquipement);
+        Exception msg = assertThrows(IllegalArgumentException.class, () -> equipementService.createEquipement(equipement));
+        System.out.println(msg.getMessage());
     }
     @Test
     public void test_save_equipement_all_fields_filled() {
@@ -48,13 +53,8 @@ class EquipementServiceTest {
         // Create a new Equipement object with only required fields filled
         Equipement equipement = new Equipement("Test Equipement", null, new Date(), new Date());
 
-        // Call the createEquipement method
-        equipementService.createEquipement(equipement);
-
-        // Retrieve the saved Equipement object from the database
-        Equipement savedEquipement = equipementRepository.getById(equipement.getId());
-
-        // Assert that the saved Equipement object is not null
-        assertNotNull(savedEquipement);
+        // Use assertThrows to check if an IllegalArgumentException is thrown
+        Exception msg = assertThrows(IllegalArgumentException.class, () -> equipementService.createEquipement(equipement));
+        System.out.println(msg.getMessage());
     }
 }
--- src/test/java/com/resourciumoptimarh/services/UserServiceTest.java
+++ src/test/java/com/resourciumoptimarh/services/UserServiceTest.java
@@ -14,16 +14,16 @@ class UserServiceTest {
     @Test
     public void test_null_email() {
 
-        UserRepository userRepository = mock(UserRepository.class);
+
         UserService userService = new UserService();
-        //userService.setUserRepository(userRepository);
+
         Role role1=new Role("Employee");
         User user = new User("John", "Doe", null, "password",role1);
 
         // Act & Assert
         Exception msg = assertThrows(IllegalArgumentException.class, () -> userService.save(user), "Email is required");
         System.out.println(msg.getMessage());
-       // verify(userRepository, never()).save(user);
+
     }
 
 }
